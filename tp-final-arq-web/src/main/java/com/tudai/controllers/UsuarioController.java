11efab81df17a63eb9029500c93442c52572f1fd
Manuel Garcia Amaro <manuelgarcia0952@gmail.com>
2020-10-23 19:41:38 -0300

    Usuario: get by id

--- tp-final-arq-web/src/main/java/com/tudai/controllers/UsuarioController.java
+++ tp-final-arq-web/src/main/java/com/tudai/controllers/UsuarioController.java
@@ -1,10 +1,13 @@
 package com.tudai.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,12 @@ public class UsuarioController extends Controller {
     public Iterable<Usuario> getUsuario() {
     	return repository.findAll();
     }
+    
+    @GetMapping("/{id}")
+    public Optional<Usuario> getUsuario(@PathVariable int id) {
+    	return repository.findById(id);
+    }
+    
     
     @PostMapping("/")
     public Usuario newUsuario(@RequestBody Usuario u) {
