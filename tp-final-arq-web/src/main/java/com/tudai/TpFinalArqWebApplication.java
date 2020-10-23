5e398986095d8241f4526c4d90da54cca382304d
Luciano D'Agata <martinvilla481@gmail.com>
2020-10-23 16:48:54 -0300

    Iniciando el TP_FINAL

---
+++ tp-final-arq-web/src/main/java/com/tudai/TpFinalArqWebApplication.java
@@ -0,0 +1,13 @@
+package com.tudai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalArqWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalArqWebApplication.class, args);
+	}
+
+}
---
+++ tp-final-arq-web/src/test/java/com/tudai/TpFinalArqWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tudai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalArqWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
