dd83184e22ead9f674afddf78d2201f28678544a
Manuel Garcia Amaro <manuelgarcia0952@gmail.com>
2020-10-23 19:59:09 -0300

    Usuario - Update - Delete

--- tp-final-arq-web/src/main/java/com/tudai/controllers/UsuarioController.java
+++ tp-final-arq-web/src/main/java/com/tudai/controllers/UsuarioController.java
@@ -3,12 +3,16 @@ package com.tudai.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,10 +41,36 @@ public class UsuarioController extends Controller {
     public Optional<Usuario> getUsuario(@PathVariable int id) {
     	return repository.findById(id);
     }
-    
-    
+        
     @PostMapping("/")
     public Usuario newUsuario(@RequestBody Usuario u) {
         return repository.save(u);
+    }
+    
+    @PutMapping("/{id}")
+    Usuario replacePerson(@RequestBody Usuario newUsuario, @PathVariable Integer id,HttpServletResponse response) {
+
+    	Optional<Usuario> usuario = repository.findById(id);
+    	if( usuario.isPresent() ) {
+    		usuario.get().setNombre(newUsuario.getNombre());
+    		return repository.save(usuario.get());    		
+    	}
+    	
+    	this.responseStatus(404,response);
+    	
+        return null;
+    }
+    
+
+    @DeleteMapping("/{id}")
+    void deleteUsuario(@PathVariable Integer id,HttpServletResponse response) {
+    	Optional<Usuario> usuario = repository.findById(id);
+    	if( usuario.isPresent() ) {
+    		repository.deleteById(id);  
+    		this.responseStatus(200,response);
+    	}
+    	else {
+    		this.responseStatus(404,response);    		
+    	}
     }
 }
